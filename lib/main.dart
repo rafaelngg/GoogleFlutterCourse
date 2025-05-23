3d76ea34eade1fb7f880bb129f78f397a0c093fb
rafaelngg <RafaelNg117@gmail.com>
2025-05-23 09:30:30 +0800

    Initial project setup with random number generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text("Ask Me Anything"),
+      ),
+      body: BallImage(),
+    );
+  }
+}
+
+class BallImage extends StatefulWidget {
+  const BallImage({Key? key}) : super(key: key);
+
+  @override
+  State<BallImage> createState() => _BallImageState();
+}
+
+class _BallImageState extends State<BallImage> {
+  int ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FloatingActionButton(
+        onPressed: () {
+          ballNumber = Random().nextInt(5);
+          print(ballNumber);
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: HomePage(),
       ),
     );
